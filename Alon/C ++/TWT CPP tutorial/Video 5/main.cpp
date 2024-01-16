82850717886ade1d0951d26466913824055da8e5
Light-The-Dark <153743837+Light-The-Dark@users.noreply.github.com>
2024-01-16 11:12:33 +0200

    Tutorial

---
+++ Alon/C ++/TWT CPP tutorial/Video 5/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+
+int x = 5;
+int y = 9;
+x += 2;
+y ++;
+x += y;
+float sum = x + y + 2;
+
+cout << sum;
+cout << x % y;
+
+
+}
