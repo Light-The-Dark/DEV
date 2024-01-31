a4367ad73998bb6a3ecdf744b00d3a8e16f9b1bc
Light-The-Dark <153743837+Light-The-Dark@users.noreply.github.com>
2024-01-31 16:38:30 +0200

    Misc

--- My stuff/Project/main.py
+++ My stuff/Project/main.py
@@ -1,9 +1,11 @@
-## TODO A 1: Get SN of current device 2: Create table for each device 3: Append the data from the files to the DB
+## TODO A 1: Get SN of current device 2: Append the data from the files to the DB
 ## TODO B 1: API calls 2: Users with permissions
 
 import pandas
+import bs4 as BeautifulSoup
 import os
 import mysql.connector
+import re
 
 db = mysql.connector.connect(
     host = "localhost",
@@ -17,26 +19,46 @@ directory = r"C:\Users\alazarix\Files\Project"
 # column_names = ["Feature_Title", "Enabled", "Feature_Description", "Part_Number"]
 
 cursor = db.cursor()
-query = "INSERT INTO wh_data (enabled) VALUES (%s)"
 
 for fn in os.listdir(directory):
     file_path = os.path.join(directory, fn)
-    with open(file_path) as f:
+    with open(file_path, "r") as f:
+
+
+        # Reads the html and parses data so it can be added to sql database
         html_content = pandas.read_html(f)[0]
-        columns = list(html_content.columns)
+        html_content = html_content.where(pandas.notna(html_content), None)
         html_content = html_content.to_dict(orient="records")
-        print(html_content[9][1])
-    for i in range(len(html_content)):
-        cursor.execute(query,(html_content[i][1],))
+
         
-# columns = list(html_list)
-# print(html_content[0][2])
+    with open(file_path, "r") as f2:
+        # Gets the specific SN
+        soup = BeautifulSoup.BeautifulSoup(f2, 'html.parser')
+        # print(soup)
+        pattern = re.compile(r'Serial Number (\d+)', re.IGNORECASE)
+
+        element_with_serial = soup.find(string=pattern)
+
+        # Extract the Serial Number using regular expression
+        match = pattern.search(str(element_with_serial))
+        serial_number = match.group(1)
+        print("Serial Number:", serial_number)
+
+
+
+    for i in range(len(html_content)):
+        cursor.execute(
+        "INSERT INTO wh_data (Feature_Title, Enabled, Feature_Description, Part_Number, SN) VALUES (%s, %s, %s, %s, %s)",
+        (html_content[i][0], html_content[i][1], html_content[i][2], html_content[i][3], serial_number)
+    )
+        
+        db.commit()
 
 
 
 
-
-# cursor.execute("INSERT INTO Person (name, age) VALUES (%s, %s)", ("Tim", 20))
+cursor.close()
+db.close()
 
 
 
--- My stuff/Project/misc.py
+++ My stuff/Project/misc.py
@@ -1,13 +1,35 @@
-from sqlalchemy import create_engine, MetaData, Table
+# from sqlalchemy import create_engine, MetaData, Table
 
-# Replace 'your_username', 'your_password', and 'your_database_name' with your MySQL credentials
-# Assuming your MySQL server is running locally on the default port (3306)
-database_connection_string = 'mysql://root:root@localhost/new_schema'
+# # Replace 'your_username', 'your_password', and 'your_database_name' with your MySQL credentials
+# # Assuming your MySQL server is running locally on the default port (3306)
+# database_connection_string = 'mysql://root:root@localhost/new_schema'
 
-engine = create_engine(database_connection_string)
-metadata = MetaData()
+# engine = create_engine(database_connection_string)
+# metadata = MetaData()
 
-# Replace 'your_table_name' with the actual name of your table
-your_table = Table('WH_Data', metadata, autoload_with=engine)
+# # Replace 'your_table_name' with the actual name of your table
+# your_table = Table('WH_Data', metadata, autoload_with=engine)
 
-print("Column Names:", your_table.columns.keys())
+# print("Column Names:", your_table.columns.keys())
+
+
+# Execute the query
+import mysql.connector
+
+db = mysql.connector.connect(
+    host = "localhost",
+    user = "root",
+    password = "root",
+    database = "new_schema",
+)
+
+cursor = db.cursor()
+
+cursor.execute("SELECT * FROM wh_data")
+
+# Fetch all the rows
+rows = cursor.fetchall()
+
+# Print the retrieved data
+for row in rows:
+    print(row)
