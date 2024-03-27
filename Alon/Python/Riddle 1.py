2443f4b5217da0f60381897a7689c452bb843aad
Light-The-Dark <153743837+Light-The-Dark@users.noreply.github.com>
2024-03-27 16:20:57 +0200

    Misc

--- Alon/Python/Riddle 1.py
+++ Alon/Python/Riddle 1.py
@@ -1,17 +1,33 @@
 def solution_1(arr):
     highest = max(arr)
+    lowest = min(arr)
 
-    for _ in arr:
-        if highest - 1 in arr and highest > 0:
-            highest -= 1
-
-    if highest == -1:
+    if highest <= 0:
         highest = 2
-        
+    else:
+        for _ in arr:
+            if highest - 1 in arr:
+                highest -= 1
+            elif highest == lowest:
+                highest = max(arr) + 2
+            else:
+                break
+
     print(highest - 1)
 
-arra = [2, 5, 1, 7, 10, 9, 8, -2, -3]
-# arra = [-1, -2, -3]
+
+def solution_2(arr):
+    highest = max(arr)
+    lowest = min(arr)
+
+    if highest <= 0:
+        highest = 2  
+
+
+
+# arra = [1, 2, 3, 4, 5]
+# arra = [2, 5, 1, 7, 10, 9, 8, -2, -3]
+# arra = [-1, -2, -3, -4]
 
 solution_1(arra)
 
