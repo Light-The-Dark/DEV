b18aab75bb07304cb6f0e64362aedb64f74a9295
Big Boy <aharonlazari@gmail.com>
2024-06-24 15:38:17 +0300

    weather api basic functionality

--- Udemy/Day 35 weather app API/main.py
+++ Udemy/Day 35 weather app API/main.py
@@ -1,9 +1,17 @@
+import requests
 
-api_key = "14443324aad33aa75cab8c2ddbf51b88"
+with open(r"Udemy\Day 35 weather app API\api_key.txt", "r") as file:
+    api_key = str(file.readlines()[0])
+
 city = "Israel"
 country = "Jerusalem"
 
+# API request with print to make sure it's correct format
 api_request = f"https://api.openweathermap.org/data/2.5/weather?q={city},{country}&appid={api_key}"
-
-
 print(api_request)
+
+# Print out the response as txt
+req = requests.get(api_request)
+print(req.text)
+
+
